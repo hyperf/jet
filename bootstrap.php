278060a9f1b17ef4e602ba6d622b1a1333863fe6
李铭昕 <715557344@qq.com>
2020-08-20 18:36:25 +0800

    Added travis ci.

---
+++ bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+! defined('BASE_PATH') && define('BASE_PATH', __DIR__);
+
+require_once BASE_PATH . '/vendor/autoload.php';
