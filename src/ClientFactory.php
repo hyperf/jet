2743ecb8396daecd07927f98f0a10fe2814b93bd
linji <237525603@qq.com>
2020-10-30 18:49:26 +0800

    Add NodeSelector for consul

--- src/ClientFactory.php
+++ src/ClientFactory.php
@@ -9,13 +9,14 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
+
 namespace Hyperf\Jet;
 
 use Hyperf\Jet\Exception\ClientException;
+use Hyperf\Jet\NodeSelector\NodeSelector;
 use Hyperf\Jet\ProtocolManager as PM;
 use Hyperf\Jet\ServiceManager as SM;
 use Hyperf\Jet\Transporter\AbstractTransporter;
-use Hyperf\Jet\Transporter\ConsulTransporter;
 use Hyperf\LoadBalancer\Node;
 use Hyperf\Rpc\Contract\DataFormatterInterface;
 use Hyperf\Rpc\Contract\PackerInterface;
@@ -26,11 +27,9 @@ class ClientFactory
 {
     public function create(string $service, string $protocol): AbstractClient
     {
-        [$transporter, $packer, $dataFormatter, $pathGenerator] = $this->protocolComponentGenerate($protocol);
+        [$transporter, $packer, $dataFormatter, $pathGenerator, $nodeSelector] = $this->protocolComponentGenerate($protocol);
 
-        $this->selectNodesForTransporter($transporter, $service, $protocol);
-
-        $this->adapterConsul($transporter, $service);
+        $this->selectNodesForTransporter($transporter, $nodeSelector, $service, $protocol);
 
         return new class($service, $transporter, $packer, $dataFormatter, $pathGenerator) extends AbstractClient {
         };
@@ -39,23 +38,17 @@ class ClientFactory
     /**
      * @param AbstractTransporter $transporter
      */
-    protected function adapterConsul(TransporterInterface $transporter, string $service)
-    {
-        if ($transporter instanceof ConsulTransporter) {
-            $transporter->activeConsulService($service);
-        }
-    }
-
-    /**
-     * @param AbstractTransporter $transporter
-     */
-    protected function selectNodesForTransporter(TransporterInterface $transporter, string $service, string $protocol): void
+    protected function selectNodesForTransporter(TransporterInterface $transporter, $nodeSelector, string $service, string $protocol): void
     {
         // If transporter self owns load balancer , just use it.
         // else use random node from config.
         // If not exist balance load , will add transporter self host, port into the LoadBalancer.
         if ($balancer = $transporter->getLoadBalancer()) {
-            if ($balanceNodes = $this->getLoadBalancerNodes($service, $protocol)) {
+            // If use node selector, we will priority choose a node from NodeSelector
+            // the current NodeSelector support consul service.
+            if ($nodeSelector instanceof NodeSelector && ($node = $nodeSelector->selectRandomNode())) {
+                $balancer->setNodes([new Node(...$node)]);
+            } elseif ($balanceNodes = $this->getLoadBalancerNodes($service, $protocol)) {
                 $balancer->setNodes($balanceNodes);
             } else {
                 $balancer->setNodes([new Node($transporter->host, $transporter->port)]);
@@ -64,7 +57,9 @@ class ClientFactory
             return;
         }
 
-        if ($randomNodes = $this->getRandomNodes($service, $protocol)) {
+        if ($nodeSelector instanceof NodeSelector && ($node = $nodeSelector->selectRandomNode())) {
+            [$transporter->host, $transporter->port] = $node;
+        } elseif ($randomNodes = $this->getRandomNodes($service, $protocol)) {
             [$transporter->host, $transporter->port] = $randomNodes;
         }
     }
@@ -72,7 +67,7 @@ class ClientFactory
     protected function getLoadBalancerNodes($service, $protocol): array
     {
         $nodeData = SM::getService($service, $protocol)[SM::NODES] ?? [];
-        $nodes = [];
+        $nodes    = [];
         foreach ($nodeData ?? [] as [$host, $port]) {
             $nodes[] = new Node($host, $port);
         }
@@ -80,11 +75,11 @@ class ClientFactory
         return $nodes;
     }
 
-    protected function getRandomNodes($service, $protocol)
+    protected function getRandomNodes($service, $protocol): array
     {
         $nodeData = SM::getService($service, $protocol)[SM::NODES] ?? [];
 
-        if (! count($nodeData)) {
+        if (!count($nodeData)) {
             return $nodeData;
         }
 
@@ -96,27 +91,28 @@ class ClientFactory
     protected function protocolComponentGenerate($protocol): array
     {
         $protocolMetadata = PM::getProtocol($protocol);
-        $transporter = $protocolMetadata[PM::TRANSPORTER] ?? null;
-        $packer = $protocolMetadata[PM::PACKER] ?? null;
-        $dataFormatter = $protocolMetadata[PM::DATA_FORMATTER] ?? null;
-        $pathGenerator = $protocolMetadata[PM::PATH_GENERATOR] ?? null;
+        $transporter      = $protocolMetadata[PM::TRANSPORTER] ?? null;
+        $packer           = $protocolMetadata[PM::PACKER] ?? null;
+        $dataFormatter    = $protocolMetadata[PM::DATA_FORMATTER] ?? null;
+        $pathGenerator    = $protocolMetadata[PM::PATH_GENERATOR] ?? null;
+        $nodeSelector     = $protocolMetadata[PM::NODE_SELECTOR] ?? null;
 
-        if (! $transporter instanceof TransporterInterface) {
+        if (!$transporter instanceof TransporterInterface) {
             throw new ClientException(sprintf('The protocol of %s transporter is invalid.', $protocol));
         }
 
-        if (! $packer instanceof PackerInterface) {
+        if (!$packer instanceof PackerInterface) {
             throw new ClientException(sprintf('The protocol of %s packer is invalid.', $protocol));
         }
 
-        if (! $dataFormatter instanceof DataFormatterInterface) {
+        if (!$dataFormatter instanceof DataFormatterInterface) {
             throw new ClientException(sprintf('The protocol of %s is data formatter invalid.', $protocol));
         }
 
-        if (! $pathGenerator instanceof PathGeneratorInterface) {
+        if (!$pathGenerator instanceof PathGeneratorInterface) {
             throw new ClientException(sprintf('The protocol of %s is path generator invalid.', $protocol));
         }
 
-        return [$transporter, $packer, $dataFormatter, $pathGenerator];
+        return [$transporter, $packer, $dataFormatter, $pathGenerator, $nodeSelector];
     }
 }
