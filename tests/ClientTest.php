fe40f49887de2795162491e9914a4fcd7c9427a5
李铭昕 <715557344@qq.com>
2023-01-08 14:37:16 +0800

    Fixed test cases.

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -24,7 +24,7 @@ use PHPUnit\Framework\TestCase;
  */
 class ClientTest extends TestCase
 {
-    protected function tearDown()
+    protected function tearDown(): void
     {
         Mockery::close();
     }
